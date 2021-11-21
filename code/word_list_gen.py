e6803831a4991931d2d66a2b192a27e3b703d03d
Megha Bose <megh.bose@gmail.com>
2021-11-21 12:33:33 +0530

    added 10 sets each of randomly sampled hflong, hfshort, lflong and lfshort words

--- code/word_list_gen.py
+++ code/word_list_gen.py
@@ -1,62 +1,73 @@
 import pandas as pd
 import random
 
-inp_file = "../data/data.csv"
+inp_file = "../data/SHABD_DATABASE.csv"
 out_word_file = "../data/words.txt"
-out_long_word_file = "../data/long_words.txt"
-out_short_word_file = "../data/short_words.txt"
+out_hflong_word_file = "../data/hflong_words.txt"
+out_lflong_word_file = "../data/lflong_words.txt"
+out_hfshort_word_file = "../data/hfshort_words.txt"
+out_lfshort_word_file = "../data/lfshort_words.txt"
 
-def get_devanagari(word):
-    return word.encode('iso-8859-1', 'ignore').decode('utf8', 'ignore')
+# def get_devanagari(word):
+#     return word.encode('iso-8859-1', 'ignore').decode('utf8', 'ignore')
+
+def get_len_threshold():
+    filename = "../data/data.csv"
+    df = pd.read_csv(filename)
+    long_word_df = df.loc[df['label'] == 'hflong']
+    long_len_list = long_word_df['length']
+    long_word_df = df.loc[df['label'] == 'lflong']
+    long_len_list.append(long_word_df['length'])
+    return min(long_len_list)
+
+def get_freq_threshold():
+    filename = "../data/data.csv"
+    df = pd.read_csv(filename)
+    hf_word_df = df.loc[df['label'] == 'hflong']
+    hf_list = hf_word_df['freq_sh']
+    hf_word_df = df.loc[df['label'] == 'hfshort']
+    hf_list.append(hf_word_df['freq_sh'])
+    return min(hf_list)
+
+def write_samples(word_list, out_file, num):
+    word_list = random.sample(list(word_list), num)
+    f = open(out_file, "w")
+    for word in word_list:
+        f.write(str(word))
+        f.write("\n")
+    f.close()
+
+len_thresh = get_len_threshold()
+freq_thresh = get_freq_threshold()
+print('Length Threshold: ',len_thresh)
+print('Frequency Threshold: ',freq_thresh)
 
 df = pd.read_csv(inp_file)
 
-# get words
-word_df = df.loc[df['label'] != 'nw']
-word_list = word_df['word']
+num = 10
 
-f = open(out_word_file, "w")
+# hf
+hf_word_df = df.loc[df['frequency'] >= freq_thresh]
 
-for word in word_list:
-    word_in_devanagari = get_devanagari(word)
-    f.write(str(word_in_devanagari))
-    f.write("\n")
+# hflong
+hflong_word_df = hf_word_df.loc[hf_word_df['total_length'] >= len_thresh]
+hflong_word_list = hflong_word_df['word']
+write_samples(hflong_word_list, out_hflong_word_file, num)
 
-f.close()
+# hfshort
+hfshort_word_df = hf_word_df.loc[hf_word_df['total_length'] < len_thresh]
+hfshort_word_list = hfshort_word_df['word']
+write_samples(hfshort_word_list, out_hfshort_word_file, num)
 
+# lf
+lf_word_df = df.loc[df['frequency'] < freq_thresh]
 
-num = 15
+# lflong
+lflong_word_df = lf_word_df.loc[lf_word_df['total_length'] >= len_thresh]
+lflong_word_list = lflong_word_df['word']
+write_samples(lflong_word_list, out_lflong_word_file, num)
 
-word_df = df.loc[df['label'] == 'hfshort']
-word_list = word_df['word']
-word_df = df.loc[df['label'] == 'lfshort']
-word_list.append(word_df['word'])
-
-short_word_list = random.sample(list(word_list), num)
-
-f = open(out_short_word_file, "w")
-
-for word in short_word_list:
-    word_in_devanagari = get_devanagari(word)
-    f.write(str(word_in_devanagari))
-    f.write("\n")
-
-f.close()
-
-
-word_df = df.loc[df['label'] == 'hflong']
-word_list = word_df['word']
-word_df = df.loc[df['label'] == 'lflong']
-word_list.append(word_df['word'])
-
-long_word_list = random.sample(list(word_list), num)
-
-f = open(out_long_word_file, "w")
-
-for word in long_word_list:
-    word_in_devanagari = get_devanagari(word)
-    f.write(str(word_in_devanagari))
-    f.write("\n")
-
-f.close()
-
+# lfshort
+lfshort_word_df = lf_word_df.loc[lf_word_df['total_length'] < len_thresh]
+lfshort_word_list = lfshort_word_df['word']
+write_samples(lfshort_word_list, out_lfshort_word_file, num)
